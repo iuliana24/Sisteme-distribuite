bd5dfda7462195372bb33fe97aad1a7ff0b4f81a
dianaG <115458163+moonchildiana@users.noreply.github.com>
2023-12-18 23:26:35 +0200

    Update PeripherialNode3.java

--- PeripherialNode3.java
+++ PeripherialNode3.java
@@ -4,24 +4,85 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class PeripherialNode3 {
-    public static void main(String[] args) {
+    private Socket socket;
+    private PrintWriter out;
+    private Scanner in;
+    private Scanner userInputScanner;
+
+    public PeripherialNode3() {
+        userInputScanner = new Scanner(System.in);
+
         try {
-            Socket socket = new Socket("127.0.0.1", 12345);
+            socket = new Socket("127.0.0.1", 12345);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new Scanner(socket.getInputStream());
+            System.out.println("Conectat la HubNode");
 
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            Scanner in = new Scanner(socket.getInputStream());
+            new Thread(() -> {
+                while (in.hasNextLine()) {
+                    String message = in.nextLine();
+                    System.out.println("HubNode: " + message);
+                }
+            }).start();
 
-            // Primirea unui mesaj de la HubNode
-            String message = in.nextLine();
-            System.out.println("HubNode spune: " + message);
-
-            // Trimite un mesaj către HubNode
-            out.println("Mesaj de la nod periferic 3!");
-
-            socket.close();
-
+            startUserInput();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void startUserInput() {
+        while (true) {
+            printMenu();
+            int option = userInputScanner.nextInt();
+            userInputScanner.nextLine(); // Consumăm newline
+
+            switch (option) {
+                case 1:
+                    sendRequest("Adăugare");
+                    break;
+                case 2:
+                    sendRequest("Ștergere");
+                    break;
+                case 3:
+                    sendRequest("Actualizare");
+                    break;
+                case 4:
+                    sendRequest("Stergere");
+                    break;
+                case 0:
+                    System.out.println("Deconectare de la HubNode.");
+                    out.println("exit");
+                    try {
+                        socket.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    return;
+                default:
+                    System.out.println("Opțiune invalidă.");
+            }
+        }
+    }
+
+    private void printMenu() {
+        System.out.println("Selectați acțiunea dorită:");
+        System.out.println("1. Cautare termen");
+        System.out.println("2. Adaugare termen");
+        System.out.println("3. Modificare termen");
+        System.out.println("4. Stergere termen");
+        System.out.println("0. Deconectare");
+        System.out.print("Introduceți numărul acțiunii: ");
+    }
+
+    private void sendRequest(String action) {
+        System.out.print("Introduceți termenul pentru " + action + ": ");
+        String term = userInputScanner.nextLine();
+
+        out.println(action + ":" + term);
+    }
+
+    public static void main(String[] args) {
+        new PeripherialNode3();
     }
 }
